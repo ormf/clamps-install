0584dbecebcf4e08361d398b62554ce612c92726
ormf <junk14@selma.hfmdk-frankfurt.de>
2025-07-20 12:21:12 +0200

    fixed init.el and sly-config.el to start without errors.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,8 +4,8 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 (tool-bar-mode -1)
-(toggle-scroll-bar -1)
-(scroll-bar-mode 1)
+;;; (toggle-scroll-bar -1)
+;;; (scroll-bar-mode 1)
 ;;; start emacs server for emacsclient
 (server-start)
 (require 'recentf)
